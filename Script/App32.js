806fa0e56dab36b6b15bb44a48bc48aa80eeee0c
nelsontchanyap79 <nelsontchanyap75@gmail.com>
2023-09-12 01:47:48 +0100

    Functionnal Timer

--- Script/App32.js
+++ Script/App32.js
@@ -1,30 +1,26 @@
 // Variables Initialization
-
-  // Days
+  let centuryDays = document.querySelector(".Days .Century");
+  let currentCenturyDays = document.createElement("span");
   let dozenDays = document.querySelector(".Days .Dozen");
   let currentDozenDays = document.createElement("span");
-
   let unitDays = document.querySelector(".Days .Unit");
   let currentUnitDays = document.createElement("span");
   
-  // Hours
+
   let dozenHours = document.querySelector(".Hours .Dozen");
   let currentDozenHours = document.createElement("span");
-
   let unitHours = document.querySelector(".Hours .Unit");
   let currentUnitHours = document.createElement("span");
 
-  // Minutes
+
   let dozenMinutes = document.querySelector(".Minutes .Dozen");
   let currentDozenMinutes = document.createElement("span");
-
   let unitMinutes = document.querySelector(".Minutes .Unit");
   let currentUnitMinutes = document.createElement("span");
 
-  // Seconds
+  
   let dozenSeconds = document.querySelector(".Seconds .Dozen");
   let currentDozenSeconds = document.createElement("span");
-
   let unitSeconds = document.querySelector(".Seconds .Unit");
   let currentUnitseconds = document.createElement("span");
 
@@ -32,16 +28,9 @@
 
 // algorithm  
 
-  currentDozenHours.setAttribute("class", "currentValue");
-  currentDozenHours.textContent = "";
-
-  dozenHours.appendChild(currentDozenHours);
-  setTimeout(() => {
-    dozenHours.removeChild(currentDozenHours);
-  }, 700);
-
 
   let clock = new Date("2024-01-31 00:00:00").getTime()
+
 
   function timer(data){
 
@@ -77,12 +66,107 @@
   setInterval(()=>{
 
     let time = timer(clock)
+ 
+    let days = time.days
+    let daysString = days.toString().length
+    let daysCentury , daysDozen , daysUnit = "0"
+  
+    let daysTable  = []
 
-    let days = timer.days
-    let hours = timer.hours
-    let minutes = timer.minutes
-    let seconds = timer.seconds
+    switch(daysString){
+      case 2 :
+        daysTable[0] = days.toString()[0]
+        daysTable[1] = days.toString()[1]
+        daysTable.unshift("0")
+        daysCentury = daysTable[0]
+        daysDozen = daysTable[1]
+        daysUnit = daysTable[2]
+        break
+      case 1 :
+        daysTable[0] = days.toString()[0]
+        daysTable.unshift("0")
+        daysTable.unshift("0")
+        daysCentury = daysTable[0]
+        daysDozen = daysTable[1]
+        daysUnit = daysTable[2]
+        break
+      default :
+        daysCentury = days.toString()[0]
+        daysDozen = days.toString()[1]
+        daysUnit = days.toString()[2]
+        break
+    }
 
+    let hours = time.hours
+    hoursString = hours.toString().length
+    let hoursDozen , hoursUnit = "0"
+
+    let hoursTable = []
+
+    switch(hoursString){
+      case 1 :
+        hoursTable[0] = hours.toString()[0]
+        hoursTable.unshift("0")
+        hoursDozen = hoursTable[0]
+        hoursUnit = hoursTable[1]
+        break
+      default :
+        hoursDozen = hours.toString()[0]
+        hoursUnit = hours.toString()[1]
+        break
+    }
+
+    let minutes = time.minutes
+    minutesString = minutes.toString().length
+    let minutesDozen , minutesUnit = "0"
+
+    let minutesTable = []
+
+    switch(minutesString){
+      case 1 :
+        minutesTable[0] = minutes.toString()[0]
+        minutesTable.unshift("0")
+        minutesDozen = minutesTable[0]
+        minutesUnit = minutesTable[1]
+        break
+      default :
+        minutesDozen = minutes.toString()[0]
+        minutesUnit = minutes.toString()[1]
+        break
+    }
+  
+
+    let seconds = time.seconds
+    secondsString = seconds.toString().length
+    let secondsDozen , secondsUnit = "0"
+
+    let secondsTable = []
+
+    switch(secondsString){
+      case 1 :
+        secondsTable[0] = seconds.toString()[0]
+        secondsTable.unshift("0")
+        secondsDozen = secondsTable[0]
+        secondsUnit = secondsTable[1]
+        break
+      default :
+        secondsDozen = seconds.toString()[0]
+        secondsUnit = seconds.toString()[1]
+        break
+    }
+
+    centuryDays.textContent = daysCentury
+    dozenDays.textContent = daysDozen
+    unitDays.textContent = daysUnit
+    
+    dozenHours.textContent = hoursDozen
+    unitHours.textContent = hoursUnit
+  
+    dozenMinutes.textContent = minutesDozen
+    unitMinutes.textContent = minutesUnit
+    
+    dozenSeconds.textContent = secondsDozen
+    unitSeconds.textContent = secondsUnit
     
 
   },700)
